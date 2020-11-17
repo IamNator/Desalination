25c27787c9f9f7cc346f049e8552c1e5bd3046fe
Nator Verinumbe <natverior1@gmail.com>
2020-11-17 09:09:56 +0100

    adjusted the code

--- Desalination.ino
+++ Desalination.ino
@@ -24,26 +24,20 @@
 
 #define BOILER 6 //digital Output 
 
-#define BAUD 9600 
+#define BAUD 115200 
 
- 
-#define WLR_FULL 100 //Water Level Reserve Full
-#define WLS_FULL 100 //Storage capacity of Tank  default = 100cm
+//Variable
+#define WLR_FULL 1 //Water Level Reserve Full
+#define WLS_FULL 1 //Water Level Storage Full
 
 #define WLB_FULL 1 //Water Level Boiler (when boiler is full)
 #define WLB_EMPTY 0 //When Boiler is empty
-#define WLB_NORMAL 56 //When it's neither full not empty
+#define WLB_NORMAL -1 //When it's neither full not empty
 
-
-<<<<<<< HEAD
-// int isfull = 0;
-bool isNotBoilerFull = true;
-=======
 float time = 0;
 float distance = 0; 
 int isfull = 0;
 bool isBoilerFull = false;
->>>>>>> 2c002f8f15dc34495d38e5577464b385d22cc4b7
 
 void setup() {
   // put your setup code here, to run once:
@@ -59,35 +53,22 @@ void setup() {
   
   pinMode(TEMP_SENSOR, INPUT);  //analog Input
 
-  //pinMode(PUMP_TO_RESERVE, OUTPUT);  //digital Output
-   pinMode(VALVE_TO_BOILER, OUTPUT); //digital Output
-   pinMode(PUMP_TO_MEMBRANE, OUTPUT); //digital Output
-   pinMode(BOILER, OUTPUT);//digital Output
+  pinMode(PUMP_TO_RESERVE, OUTPUT);  //digital Output
+  pinMode(VALVE_TO_BOILER, OUTPUT); //digital Output
+  pinMode(PUMP_TO_MEMBRANE, OUTPUT); //digital Output
+  pinMode(BOILER, OUTPUT);//digital Output
   
 }
 
-volatile float wl_reserve;
-volatile float wl_boiler; 
-volatile float boiler_temp;
-
 void loop() {
-  wl_reserve = WLReserve();
-  wl_boiler = WLBoiler();
-
-  Serial.print("WLReserve : ");
-  Serial.println(wl_reserve);
-
-//**************************For Filling up Reserve Tank*********************
   
-  if (wl_reserve < 90){  //When water level is less then 90cm/100cm of Reservoir Tank
+  // put your main code here, to run repeatedly:
+  if (WLReserve() < WLR_FULL){
     digitalWrite(PUMP_TO_RESERVE, HIGH);
   } else {
     digitalWrite(PUMP_TO_RESERVE, LOW);
   }
-<<<<<<< HEAD
-  
-=======
-//ERROR--needs adjustments
+//ERROR FIXED
   if ( (WLBoiler() == WLB_EMPTY) ||  !(isBoilerFull)  ){ //Tank is only filled up when it's empty
     digitalWrite(VALVE_TO_BOILER, HIGH);
     if (WLBoiler() == WLB_FULL){
@@ -101,37 +82,20 @@ void loop() {
   int boiler_temp = BoilerTemp();
   Serial.print("WLBoiler : ");
   Serial.println(wl_boiler);
->>>>>>> 2c002f8f15dc34495d38e5577464b385d22cc4b7
 
-// //*****************************For Filling up Boiler Tank when Empty****************
-//   if ( wl_boiler == WLB_EMPTY){
-//      isNotBoilerFull = true;
-//     }
-
-//   if ( (wl_boiler == WLB_EMPTY) ||  (isNotBoilerFull)  ){ //Tank is only filled up when it's empty
-//     digitalWrite(VALVE_TO_BOILER, HIGH);
-//     if (wl_boiler == WLB_FULL){
-//      isNotBoilerFull = false;
-//     }
-//   } else {
-//     digitalWrite(VALVE_TO_BOILER, LOW);
-//   }
-
-//   //*************Boiler Temperature********************
-
-//   if( (wl_boiler != WLB_EMPTY) && ( boiler_temp < 95) ){
-//     digitalWrite(BOILER, HIGH);
-//   } else {
-//     digitalWrite(BOILER, LOW);
-//   }
+  Serial.print("Boiler Temp : ");
+  Serial.println(boiler_temp);
+  if( (wl_boiler != WLB_EMPTY) && (boiler_temp < 95) ){
+    digitalWrite(BOILER, HIGH);
+  } else {
+    digitalWrite(BOILER, LOW);
+  }
   
-// //*******************Pump to Membrane ***********************
-//    if( (boiler_temp >= 95/*C*/) && (WLStorage() < 90/*cm*/) && (wl_boiler != WLB_EMPTY) ){
-//      digitalWrite(PUMP_TO_MEMBRANE, HIGH);
-//   } else {
-//     digitalWrite(PUMP_TO_MEMBRANE, LOW);
-//   }
-
+  if( (BoilerTemp() >= 95) && (WLStorage() < WLS_FULL) && (WLBoiler() != WLB_EMPTY) ){
+     digitalWrite(PUMP_TO_MEMBRANE, HIGH);
+  } else {
+    digitalWrite(PUMP_TO_MEMBRANE, LOW);
+  }
 
 }
 
@@ -139,17 +103,13 @@ void loop() {
 
 uint8_t BoilerTemp(){
   uint8_t temp = 0;
-  temp = (analogRead(TEMP_SENSOR) * 51 /105); //Reads and converts from farenheit to celcius
- // Serial.print("Boiler Temp : ");
- // Serial.println(temp);
+  temp = ( analogRead(TEMP_SENSOR) - 32 ) / 1.8 ; //Reads and converts from farenheit to celcius
   return temp; 
 }
 
 
 uint8_t WLReserve(){ //Water Level Reserve
-volatile float time = 0;
-volatile float distance = 0;
-  
+   uint8_t water_level = WLR_FULL; 
    digitalWrite(WL_RESERVE_SENSOR_TR,LOW);
    delayMicroseconds(2);
    digitalWrite(WL_RESERVE_SENSOR_TR,HIGH);
@@ -158,24 +118,18 @@ volatile float distance = 0;
    delayMicroseconds(2);
    time=pulseIn(WL_RESERVE_SENSOR_ECHO,HIGH);
    
-   distance=time*350/20000;
- return (distance);
+   distance=time*340/20000;
+ return (water_level - distance);
 }
 
 
-int WLBoiler(){ //Water Level Boiler
-
-  bool level_high = digitalRead(WL_BOILER_FULL);
-  bool level_low = digitalRead(WL_BOILER_EMPTY);
+uint8_t WLBoiler(){ //Water Level Boiler
   
-  if(level_low){ //the pin is grounded when it's active
-   // Serial.println("WL_BOILER_EMPTY ");
+  if(digitalRead(WL_BOILER_EMPTY)){ //the pin is grounded when it's active
     return WLB_EMPTY;
-  } else if(level_high == 0){ //the pin is grounded when it's active
-  //  Serial.println("WL_BOILER_FULL");
+  } else if(!digitalRead(WL_BOILER_FULL)){ //the pin is grounded when it's active
     return WLB_FULL;
   } else {
-  //  Serial.println("WLB_NORMAL");
     return WLB_NORMAL;
   }
 
@@ -183,9 +137,7 @@ int WLBoiler(){ //Water Level Boiler
 
 
 uint8_t WLStorage(){ //Water Level Storage
-  volatile float time = 0;
-  volatile float distance = 0; 
-
+  uint8_t water_level = WLS_FULL;
    digitalWrite(WL_STORAGE_SENSOR_TR,LOW);
    delayMicroseconds(2);
    digitalWrite(WL_STORAGE_SENSOR_TR,HIGH);
@@ -193,14 +145,8 @@ uint8_t WLStorage(){ //Water Level Storage
    digitalWrite(WL_STORAGE_SENSOR_TR,LOW);
    delayMicroseconds(2);
    time=pulseIn(WL_STORAGE_SENSOR_ECHO,HIGH);
-
-    distance=time*350/20000;
-  //  Serial.print("Storage Tank : ");
-  //  Serial.println(distance);
-  return (WLS_FULL - distance);
+   
+   distance=time*340/20000;
+  return (water_level - distance);
 }
 
-
-
-
- 
